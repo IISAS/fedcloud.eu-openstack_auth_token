2ac09f2052ec81cac61a3944ecd4e4a85da981c8
root <root@viet.novalocal>
2015-11-04 10:05:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
-from distutils.core import setup
+#!/usr/bin/python
+
+from setuptools import setup
+
 setup(name='openstack_auth_token',
       version='1.0',
-      py_modules=['openstack_auth_token'],
+      description='Module for logging into Horizon via token',
+      author='Viet Tran',
+      author_email='tdviet@gmail.com',
+      url='https://github.com/tdviet/openstack_auth_token',
+      packages=['openstack_auth_token'],
+      package_data={'openstack_auth_token': ['templates/*']},
+      license='MIT',
+      zip_safe=False,
+      install_requires=['openstack_auth',]
       )
+
